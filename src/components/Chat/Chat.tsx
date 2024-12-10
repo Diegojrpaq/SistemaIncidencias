8d4c1f40814e78210eff2fc1404522e9e9d7bc9b
CrisH <cristopher.26hp@gmail.com>
2024-12-10 09:53:20 -0600

    Nuevas modificaciones al chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -2,31 +2,28 @@ import React from 'react'
 
 const Chat = () => {
     return (
-        <div className='bg-indigo-500'>
-            <h3> Hoolichat</h3>
-            <div>Chat...</div>
-            <div className="bg-gray-200 h-screen p-4 rounded-lg shadow-lg">
-                <div>Mensaje 1</div>
-                <div>Mensaje 2</div>
-                <div>Mensaje 3</div>
-                <div>Mensaje 4</div>
-                <div>Mensaje 1</div>
-                <div>Mensaje 2</div>
-                <div>Mensaje 3</div>
-                <div>Mensaje 4</div>
-                <div>Mensaje 1</div>
-                <div>Mensaje 2</div>
-                <div>Mensaje 3</div>
-                <div>Mensaje 4</div>
-                <div>Mensaje 1</div>
-                <div>Mensaje 2</div>
-                <div>Mensaje 3</div>
-                <div>Mensaje 4</div>
-                <div>Mensaje 1</div>
-                <div>Mensaje 2</div>
-                <div>Mensaje 3</div>
-                <div>Mensaje 4</div>
-            </div>
+
+        <div className="h-screen">
+            <div>Mensaje 1</div>
+            <div>Mensaje 2</div>
+            <div>Mensaje 3</div>
+            <div>Mensaje 4</div>
+            <div>Mensaje 1</div>
+            <div>Mensaje 2</div>
+            <div>Mensaje 3</div>
+            <div>Mensaje 4</div>
+            <div>Mensaje 1</div>
+            <div>Mensaje 2</div>
+            <div>Mensaje 3</div>
+            <div>Mensaje 4</div>
+            <div>Mensaje 1</div>
+            <div>Mensaje 2</div>
+            <div>Mensaje 3</div>
+            <div>Mensaje 4</div>
+            <div>Mensaje 1</div>
+            <div>Mensaje 2</div>
+            <div>Mensaje 3</div>
+            <div>Mensaje 4</div>
         </div>
     )
 }
--- src/components/ModalIncidencia/ModalIncidencia.tsx
+++ src/components/ModalIncidencia/ModalIncidencia.tsx
@@ -8,19 +8,15 @@ import {
     ModalFooter,
     Button,
     useDisclosure,
-    Card,
-    CardHeader,
-    Divider,
-    CardBody,
-    Image,
-    Spinner, Skeleton
+    Skeleton,
+    Input,
 } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import ContentModal from "../ContentModal/ContentModal";
 import InfoGuia from "../infoGuia/InfoGuia";
 import Chat from "../Chat/Chat";
 import SkeletonInfoGuia from "../skeleton/skeletonInfoGuia";
-
+import { IoSendSharp } from "react-icons/io5";
 interface modalProps {
     incidencia: Incidencia
 }
@@ -30,10 +26,27 @@ const ModalIncidencia = ({ incidencia }: modalProps) => {
     const [inc, setInc] = useState();
     const [statusRes, setStatusRes] = useState();
     //Estado del skeleton
-    const [isLoaded, setIsLoaded] = useState(false);
+    const [valueMsg, setValueMsg] = useState("");
 
-    const toggleLoad = () => {
-        setIsLoaded(!isLoaded);
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setValueMsg(e.target.value);
+    };
+
+    // Manejador de envío
+    const handleSend = () => {
+        if (valueMsg.trim()) {
+            console.log("Texto enviado:", valueMsg); // Enivar texto
+            setValueMsg(""); // Limpia el input
+        }
+    };
+
+    // Manejador para Enter
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleSend();
+        }
     };
 
     const getData = async () => {
@@ -66,7 +79,7 @@ const ModalIncidencia = ({ incidencia }: modalProps) => {
                             <ModalBody className="flex justify-center items-center">
                                 <div className="flex">
                                     {/* Sección izquierda - Información */}
-                                    <div className="w-2/3 max-h-[700px] overflow-y-auto p-4 overflow-x-hidden scrollbar-hide">
+                                    {/* <div className="w-2/3 max-h-[700px] overflow-y-auto p-4 overflow-x-hidden scrollbar-hide">
                                         {
                                             inc && statusRes === 200 ?
                                                 <InfoGuia incidencia={inc} />
@@ -77,24 +90,105 @@ const ModalIncidencia = ({ incidencia }: modalProps) => {
                                                     :
                                                     <SkeletonInfoGuia />
                                         }
-                                    </div>
+                                    </div> */}
+
+                                    {
+                                        inc && statusRes === 200 ?
+                                            <div className="w-2/3 max-h-[700px] overflow-y-auto p-4 overflow-x-hidden scrollbar-hide">
+                                                <InfoGuia incidencia={inc} />
+                                            </div>
+                                            : statusRes === 204 ?
+                                                <div className="text-lg text-center w-96">
+                                                    No existe la guía
+                                                </div>
+                                                :
+                                                <div className="w-2/3 max-h-[700px] overflow-y-auto p-4 overflow-x-hidden scrollbar-hide">
+                                                    <SkeletonInfoGuia />
+                                                </div>
+
+                                    }
+
+
                                     {/* Sección derecha - Chat */}
-                                    <div className="w-1/3  max-h-[700px] overflow-y-auto p-4 overflow-x-hidden scrollbar-hide">
+                                    {
+                                        inc && statusRes === 200 ?
+                                            <div className="w-1/3 max-h-[700px] flex flex-col bg-gray-200 shadow-lg rounded-lg p-2">
+                                                <h3>Comentarios</h3>
+                                                <div className="overflow-y-auto p-4 overflow-x-hidden scrollbar-hide">
+                                                    <Chat />
+                                                </div>
+                                                <div className="">
+                                                    <Input
+                                                        value={valueMsg}
+                                                        type="text"
+                                                        placeholder="Send a message"
+                                                        className=""
+                                                        size="lg"
+                                                        onChange={handleInputChange}
+                                                        endContent={
+                                                            <div
+                                                                className={`
+                                                                    cursor-pointer
+                                                                    
+                                                                `}
+                                                                onClick={handleSend}
+                                                            >
+                                                                <IoSendSharp />
+                                                            </div>
+                                                        }
+                                                        onKeyDown={handleKeyDown}
+                                                    />
+                                                </div>
+                                            </div>
+                                            : statusRes === 204 ?
+                                                <div className="w-1/2 text-lg text-center w-full">
+                                                    No existe información del chat
+                                                </div> :
+                                                <>
+                                                    <div className="w-1/3 max-h-[700px] rounded-lg">
+                                                        <Skeleton className="h-full w-full rounded-md" />
+                                                    </div>
+                                                </>
+                                    }
+                                    {/* <div className="w-1/3 max-h-[700px] flex flex-col bg-gray-200 shadow-lg rounded-lg p-2">
                                         {
                                             inc && statusRes === 200 ?
-                                                <Chat />
-                                                : statusRes === 204 ?
+                                                <>
+                                                    <h3>Chat</h3>
+                                                    <div className="overflow-y-auto p-4 overflow-x-hidden scrollbar-hide">
+                                                        <Chat />
+                                                    </div>
+                                                    <div>Send message</div>
+                                                </> : statusRes === 204 ?
                                                     <div className="text-lg text-left w-96">
                                                         No existe información del chat
-                                                    </div>
-                                                    :
+                                                    </div> :
                                                     <>
-                                                        <div className="h-screen shadow-lg">
-                                                            <Skeleton className="h-screen w-full rounded-md" />
+                                                        <div className="h-full">
+                                                            <Skeleton className="h-full w-full rounded-md bg-transparent" />
                                                         </div>
                                                     </>
-                                        }
-                                    </div>
+
+                                        } */}
+                                    {/* <h3>Chat</h3>
+                                        <div className="overflow-y-auto p-4 overflow-x-hidden scrollbar-hide">
+                                            {
+                                                inc && statusRes === 200 ?
+                                                    <Chat />
+                                                    : statusRes === 204 ?
+                                                        <div className="text-lg text-left w-96">
+                                                            No existe información del chat
+                                                        </div>
+                                                        :
+                                                        <>
+                                                            <div className="h-screen shadow-lg">
+                                                                <Skeleton className="h-screen w-full rounded-md" />
+                                                            </div>
+                                                        </>
+                                            }
+                                        </div>
+                                        <div>Send message</div> */}
+                                    {/* </div> */}
                                 </div>
                             </ModalBody>
                             <ModalFooter>
