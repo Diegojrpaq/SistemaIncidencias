8dc80a50a1fbd27d18d5e6379df21cbf16e32f61
CrisH <cristopher.26hp@gmail.com>
2025-01-07 12:16:27 -0600

    Se agrego el buscador al navbar, quedo listo la parte visual

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Select, SelectItem } from "@nextui-org/react";
+import { Select, SelectItem, Input } from "@nextui-org/react";
 import { catalogoSucursales, dataUser } from '@/lib/interfaces';
 
 interface navbarProps {
@@ -43,8 +43,60 @@ const Navbar = ({ user, catalogoSucursales }: navbarProps) => {
           ))}
         </Select>
       </div>
+
+      <div>
+        <div className="w-[310px] rounded-md flex justify-center items-center">
+          <Input
+            isClearable
+            size='sm'
+            classNames={{
+              input: [
+                "bg-transparent",
+                "text-black/90 dark:text-white/90",
+                "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+              ],
+            }}
+            label="Num. Guía"
+            placeholder="Buscar por número de guía"
+            radius="sm"
+            startContent={
+              <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+            }
+          />
+        </div>
+      </div>
     </nav>
   )
 }
+
+export const SearchIcon = (props: any) => {
+  return (
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height="1em"
+      role="presentation"
+      viewBox="0 0 24 24"
+      width="1em"
+      {...props}
+    >
+      <path
+        d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      />
+      <path
+        d="M22 22L20 20"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+};
 
 export default Navbar
