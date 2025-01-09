401c5a7b12e6b89c1df74ac40b00455e4077a307
CrisH <cristopher.26hp@gmail.com>
2025-01-09 16:07:01 -0600

    Se soluciono error que daba en la consola por tener un div dentro de un p, el componente <Chip> de nextUI es un div y estaba dentro de un <p>, ahora el p se cambio por un div

--- src/components/infoGuia/InfoGuia.tsx
+++ src/components/infoGuia/InfoGuia.tsx
@@ -16,123 +16,108 @@ interface infoGuiaProps {
     chatData: chatData | undefined;
 }
 
-const InfoGuia = ({incidencia, chatData}: infoGuiaProps) => {
-  return (
-<div className="flex flex-col gap-3 
-            p-4"
-                >
+const InfoGuia = ({ incidencia, chatData }: infoGuiaProps) => {
+    return (
+        <div className="flex flex-col gap-3 p-4">
+            <Card>
+                <CardHeader className="flex gap-3">
+                    <div className="flex items-center gap-2 ml-3">
+                        <p className="text-lg">Información de la guía</p>
+                    </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                    <div className="grid grid-cols-2 gap-5 p-2">
+                        <div className="flex flex-col gap-2">
+                            <div><Chip size="lg" radius="sm">NumGuia:</Chip> {incidencia.numGuia}</div>
+                            <div><Chip size="lg" radius="sm">Origen:</Chip> {incidencia.origen}</div>
+                            <div><Chip size="lg" radius="sm">Destino:</Chip> {incidencia.destino}</div>
+                            <div><Chip size="lg" radius="sm">Cliente Origen:</Chip> {incidencia.clienteOrigenNombre}</div>
+                        </div>
+                        <div className="flex flex-col gap-2 ">
+                            <div><Chip size="lg" radius="sm">Volumen:</Chip> {incidencia.volumen} mt3</div>
+                            <div><Chip size="lg" radius="sm">Peso:</Chip> {incidencia.peso} kg</div>
+                            <div><Chip size="lg" radius="sm">Cantidad:</Chip> {incidencia.cantidad}</div>
+                            <div><Chip size="lg" radius="sm">Cliente Destino:</Chip> {incidencia.clienteDestinoNombre}</div>
+                        </div>
+                    </div>
+                </CardBody>
+            </Card>
 
-                    <Card>
-                        <CardHeader className="flex gap-3">
-                            <div className="flex items-center gap-2 ml-3">
-                                <p className="text-lg">Información de la guía</p>
-                                {/* <p className="text-md text-default-500">asdf</p> */}
-                            </div>
-                        </CardHeader>
-                        <Divider />
-                        <CardBody>
-                            <div className="grid grid-cols-2 gap-5 p-2">
-                                <div className="flex flex-col gap-2">
-                                    <p><Chip size="lg" radius="sm">NumGuia:</Chip> {incidencia.numGuia}</p>
-                                    <p><Chip size="lg" radius="sm">Origen:</Chip> {incidencia.origen}</p>
-                                    <p><Chip size="lg" radius="sm">Destino:</Chip> {incidencia.destino}</p>
-                                    <p><Chip size="lg" radius="sm">Cliente Origen:</Chip> {incidencia.clienteOrigenNombre}</p>
-                                </div>
-                                <div className="flex flex-col gap-2 ">
-                                    <p><Chip size="lg" radius="sm">Volumen:</Chip> {incidencia.volumen} mt3</p>
-                                    <p><Chip size="lg" radius="sm">Peso:</Chip> {incidencia.peso} kg</p>
-                                    <p><Chip size="lg" radius="sm">Cantidad:</Chip> {incidencia.cantidad}</p>
-                                    <p><Chip size="lg" radius="sm">Cliente Destino:</Chip> {incidencia.clienteDestinoNombre}</p>
-                                </div>
-                            </div>
-                        </CardBody>
+            <Card>
+                <CardHeader className="flex gap-3">
+                    <div className="flex items-center gap-2 ml-3">
+                        <p className="text-lg">Información de la incidencia</p>
+                    </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                    <div className="grid grid-cols-auto">
+                        <div className="flex flex-col gap-2 p-2">
+                            <div><Chip size="lg" radius="sm">Motivo incidencia:</Chip> {incidencia.descripcion}</div>
+                            <div><Chip size="lg" radius="sm">Sucursal:</Chip> {incidencia.suc_genera_incidencia}</div>
+                            <div><Chip size="lg" radius="sm">Creada por:</Chip> {incidencia.empleado_registra}</div>
+                            <div><Chip size="lg" radius="sm">Registro incidencia:</Chip> {formatDate(incidencia.fecha_registro_incidencia)}</div>
+                            <div><Chip size="lg" radius="sm">Descripcion:</Chip> {incidencia.nota}</div>
+                            <div><Chip size="lg" radius="sm">Status:</Chip> {
+                                `${incidencia.incidencia === 1 ? 'Abierta'
+                                    : ''}`
+                            }</div>
+                        </div>
+                    </div>
+                </CardBody>
+            </Card>
 
-                    </Card>
-                    
-                    <Card>
-                        <CardHeader className="flex gap-3">
+            <Card>
+                <CardHeader className="flex gap-3">
+                    <div className="flex items-center gap-2 ml-3">
+                        <p className="text-lg">Evidencia</p>
+                    </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                    <div className="flex justify-center w-full items-center">
+                        <div className="grid grid-cols-2 items-center gap-2">
+                            <Image
+                                width={200}
+                                alt="NextUI hero Image"
+                                src="https://nextui.org/images/hero-card-complete.jpeg"
+                            />
+                            <Image
+                                width={200}
+                                src="https://app.requestly.io/delay/1000/https://nextui.org/images/fruit-4.jpeg"
+                                fallbackSrc="https://via.placeholder.com/300x200"
+                                alt="NextUI Image with fallback"
+                            />
+                            <Image
+                                width={200}
+                                alt="NextUI hero Image"
+                                src="https://nextui.org/images/hero-card-complete.jpeg"
+                            />
+                            <Image
+                                width={200}
+                                alt="NextUI hero Image"
+                                src="https://nextui.org/images/hero-card-complete.jpeg"
+                            />
+                        </div>
+                    </div>
+                </CardBody>
+            </Card>
 
-                            <div className="flex items-center gap-2 ml-3">
-                                <p className="text-lg">Información de la incidencia</p>
-                                {/* <p className="text-md text-default-500">asdf</p> */}
-                            </div>
-                        </CardHeader>
-                        <Divider />
-                        <CardBody>
-                            <div className="grid grid-cols-auto">
-                                <div className="flex flex-col gap-2 p-2">
-                                    <p><Chip size="lg" radius="sm">Motivo incidencia:</Chip> {incidencia.descripcion}</p>
-                                    <p><Chip size="lg" radius="sm">Sucursal:</Chip> {incidencia.suc_genera_incidencia}</p>
-                                    <p><Chip size="lg" radius="sm">Creada por:</Chip> {incidencia.empleado_registra}</p>
-                                    <p><Chip size="lg" radius="sm">Registro incidencia:</Chip> {formatDate(incidencia.fecha_registro_incidencia)}</p>
-                                    <p><Chip size="lg" radius="sm">Descripcion:</Chip> {incidencia.nota}</p>
-                                    <p><Chip size="lg" radius="sm">Status:</Chip> {
-                                        `${incidencia.incidencia === 1 ? 'Abierta'
-                                            : ''}`
-                                    }</p>
-                                </div>
-                            </div>
-                        </CardBody>
-
-                    </Card>
-
-
-                    <Card>
-                        <CardHeader className="flex gap-3">
-
-                            <div className="flex items-center gap-2 ml-3">
-                                <p className="text-lg">Evidencia</p>
-                                {/* <p className="text-md text-default-500">asdf</p> */}
-                            </div>
-                        </CardHeader>
-                        <Divider />
-                        <CardBody>
-                            <div className="flex justify-center w-full items-center">
-                                <div className="grid grid-cols-2 items-center gap-2">
-                                    <Image
-                                        width={200}
-                                        alt="NextUI hero Image"
-                                        src="https://nextui.org/images/hero-card-complete.jpeg"
-                                    />
-                                    <Image
-                                        width={200}
-                                        // height={200}
-                                        src="https://app.requestly.io/delay/1000/https://nextui.org/images/fruit-4.jpeg"
-                                        fallbackSrc="https://via.placeholder.com/300x200"
-                                        alt="NextUI Image with fallback"
-                                    />
-                                    <Image
-                                        width={200}
-                                        alt="NextUI hero Image"
-                                        src="https://nextui.org/images/hero-card-complete.jpeg"
-                                    />
-                                    <Image
-                                        width={200}
-                                        alt="NextUI hero Image"
-                                        src="https://nextui.org/images/hero-card-complete.jpeg"
-                                    />
-
-                                </div>
-                            </div>
-                        </CardBody>
-
-                    </Card>
-
-                    <Card>
-                        <CardHeader className="flex gap-3">
-                            <div className="flex w-full justify-between items-center gap-2 ml-3">
-                                <p className="text-lg">Información Sucursales</p>
-                                <SelectSucursalAsociada />
-                            </div>
-                        </CardHeader>
-                        <Divider />
-                        <CardBody>
-                            <TableSucursales sucursales={chatData?.listSucursales}/>
-                        </CardBody>
-
-                    </Card>
-                </div>
-  )
+            <Card>
+                <CardHeader className="flex gap-3">
+                    <div className="flex w-full justify-between items-center gap-2 ml-3">
+                        <p className="text-lg">Información Sucursales</p>
+                        <SelectSucursalAsociada />
+                    </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                    <TableSucursales sucursales={chatData?.listSucursales} />
+                </CardBody>
+            </Card>
+        </div>
+    )
 }
 
 export default InfoGuia
