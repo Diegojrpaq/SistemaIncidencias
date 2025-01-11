4c1f2ee09e124f67e60121a71026ac8a5ecaa05f
legolasblack <legolas.black.ds@gmail.com>
2025-01-11 13:14:20 -0600

    se respalda el nuevo esquema de las tablas para el proyecto

--- src/documentacion/MigrationNesTables.sql
+++ src/documentacion/MigrationNesTables.sql
@@ -44,85 +44,28 @@
     FOREIGN KEY (id_permiso) REFERENCES permisos(id)
 ); */
 
-CREATE TABLE incidencia_viaje_estatus (
-    id bigint primary key generated always as identity,
-    nombre text not null,
-    descripcion text,
-    fecha_registro timestamp with time zone default now()
-);
-
-CREATE TABLE incidencia_historial_estatus (
-    id bigint primary key generated always as identity,
-    id_incidencia bigint not null,
-    id_estatus_anterior bigint,
-    id_estatus_nuevo bigint not null,
-    id_usuario bigint not null,
-    tipo_movimiento text not null,
-    fecha_cambio timestamp with time zone default now(),
-    FOREIGN KEY (id_incidencia) REFERENCES incidencias(id),
-    FOREIGN KEY (id_estatus_anterior) REFERENCES catalogo_estatus_incidencia(id),
-    FOREIGN KEY (id_estatus_nuevo) REFERENCES catalogo_estatus_incidencia(id),
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
-);
-
-
-CREATE TABLE chat_empresarial (
-    id bigint primary key generated always as identity,
-    fecha_creacion timestamp with time zone default now(),
-    nombre text not null,
-    id_incidencia bigint,
-    id_creador bigint not null,
-    is_active boolean default false,
-    fecha_borrado timestamp with time zone,
-    FOREIGN KEY (id_incidencia) REFERENCES incidencias(id),
-    FOREIGN KEY (id_creador) REFERENCES usuarios(id)
-);
-
-CREATE TABLE chat_miembro (
-    id bigint primary key generated always as identity,
-    id_chat bigint not null,
-    id_usuario bigint not null,
-    fecha_union timestamp with time zone default now(),
-    is_active boolean default true,
-    is_admin boolean default false,
-    FOREIGN KEY (id_chat) REFERENCES chat(id),
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
-);
-
-CREATE TABLE chat_mensaje (
-    id bigint primary key generated always as identity,
-    id_chat bigint not null,
-    id_user bigint not null,
-    id_tipo_mensaje bigint;
-    fecha_registro timestamp with time zone default now(),
-    is_received boolean,
-    date_received timestamp with time zone,
-    is_read boolean,
-    fecha_leido timestamp with time zone,
-    is_deleted boolean default false,
-    fecha_borrado timestamp with time zone,
-    foreign key (id_tipo_mensaje) references tipo_mensajes (id);
-    FOREIGN KEY (id_chat) REFERENCES chat(id),
-    FOREIGN KEY (id_user) REFERENCES usuarios(id)
-);
-
-create table tipo_mensajes (
-  id bigint primary key generated always as identity,
-  tipo text not null,
-  descripcion text
-);
 
 /*Version sintaxis para mariadb*/
 /* Esta version se implementara en capacitacion el dia 21/10/2024 para calar el desarrollo de las apis y websockets necesarios para el chat */
+
 
 CREATE TABLE incidencia_viaje_estatus (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT,
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    genera_columna BOOLEAN 
+);/*virificada en Capacitacion*/
 
-CREATE TABLE incidencia_historial_estatus (
+CREATE TABLE incidencia_configuracion_Alertas (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    tiempo_espera INT,
+    a_quien_notifica BIGINT
+);/*virificada en Capacitacion*/
+
+
+CREATE TABLE incidencia_log_estatus (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_incidencia BIGINT NOT NULL,
     id_estatus_anterior BIGINT,
@@ -133,9 +76,9 @@ CREATE TABLE incidencia_historial_estatus (
     FOREIGN KEY (id_estatus_anterior) REFERENCES catalogo_estatus_incidencia(id),
     FOREIGN KEY (id_estatus_nuevo) REFERENCES catalogo_estatus_incidencia(id),
     FOREIGN KEY (id_empleado_modifica) REFERENCES empleado_principal(id)
-);
+);/*virificada en Capacitacion*/
 
-CREATE TABLE chat_empresarial (
+CREATE TABLE chat_incidencias (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     nombre VARCHAR(255) NOT NULL,
@@ -146,7 +89,7 @@ CREATE TABLE chat_empresarial (
     tipo_chat BIGINT not null,
     FOREIGN KEY (id_incidencia) REFERENCES viaje_incidencia(id),
     FOREIGN KEY (id_empleado_creador) REFERENCES empleado_principal(id)
-);
+);/*virificada en Capacitacion*/
 
 CREATE TABLE chat_miembro (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -155,29 +98,63 @@ CREATE TABLE chat_miembro (
     fecha_union TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_active BOOLEAN DEFAULT TRUE,
     is_admin BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (id_chat) REFERENCES chat_empresarial(id),
+    FOREIGN KEY (id_chat) REFERENCES chat_incidencias(id),
     FOREIGN KEY (id_empleado) REFERENCES empleado_principal(id)
-);
+);/*virificada en Capacitacion*/
+
 
 CREATE TABLE chat_mensaje (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_chat BIGINT NOT NULL,
-    id_empleado BIGINT NOT NULL,
+    id_user BIGINT NOT NULL,
+    mensaje TEXT,
     id_tipo_mensaje BIGINT NOT NULL,
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_received BOOLEAN DEFAULT FALSE,
-    date_received TIMESTAMP,
+    date_received TIMESTAMP default null,
     is_read BOOLEAN DEFAULT FALSE,
-    fecha_leido TIMESTAMP,
+    fecha_leido TIMESTAMP default null,
     is_deleted BOOLEAN DEFAULT FALSE,
-    fecha_borrado TIMESTAMP,
-    FOREIGN KEY (id_tipo_mensaje) REFERENCES tipo_mensajes(id),
-    FOREIGN KEY (id_chat) REFERENCES chat_empresarial(id),
+    fecha_borrado TIMESTAMP default null,
+    mensaje_status int default 1;
+    -- FOREIGN KEY (id_tipo_mensaje) REFERENCES tipo_mensajes(id),
+    FOREIGN KEY (id_chat) REFERENCES chat_incidencias(id),
     FOREIGN KEY (id_user) REFERENCES empleado_principal(id)
-);
+);/*virificada en Capacitacion*/
 
-CREATE TABLE tipo_mensajes (
+CREATE TABLE chat_tipo_mensaje (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     tipo TEXT NOT NULL,
     descripcion TEXT
-);
+);/*virificada en Capacitacion*/
+
+CREATE TABLE chat_sucursal_involucrada (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id_chat BIGINT NOT NULL,
+    id_sucursal BIGINT NOT NULL,
+    fecha_union TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (id_chat) REFERENCES chat_incidencias(id),
+    FOREIGN KEY (id_sucursal) REFERENCES sucursal_principal(id)
+);/*virificada en Capacitacion*/
+
+
+/*Alters */
+/*modificacion para viaje_transacciones*/
+ALTER TABLE viaje_transacciones
+ADD COLUMN id_sucursal INT;  -- Ajusta el tipo de dato si es necesario
+ALTER TABLE viaje_transacciones
+ADD CONSTRAINT fk_id_sucursal
+FOREIGN KEY (id_sucursal) REFERENCES sucursal_principal(id)
+ON DELETE CASCADE ON UPDATE CASCADE;
+/*virificada en Capacitacion*/
+
+
+/*modificacion para viaje_incidencia*/
+ALTER TABLE viaje_Incidencia
+ADD COLUMN id_sucursal INT;  -- Ajusta el tipo de dato si es necesario
+ALTER TABLE viaje_Incidencia
+ADD CONSTRAINT fk_id_sucursal
+FOREIGN KEY (id_sucursal) REFERENCES sucursal_principal(id)
+ON DELETE CASCADE ON UPDATE CASCADE;
+/*tablas para permisos*/
